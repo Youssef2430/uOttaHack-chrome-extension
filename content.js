e74018d1ce6008bd2ae02ce9e6a307b14ea58dbb
Youssef30 <ychou031@uottawa.ca>
2023-02-11 14:12:42 -0500

    restructure of the code

--- content.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.sendMessage({ action: "getPrices" }, function(response) {
-  // Switch the prices
-  var prices = document.getElementsByClassName("a-offscreen");
-  for (var i = 0; i < prices.length; i++) {
-    // prices[i].innerHTML = response.prices[i];
-    prices[i].innerHTML = "400";
-  }
-});
-  
